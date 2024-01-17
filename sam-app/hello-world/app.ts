29fdf01d1081f7ff97458b54f20b4f097304973a
Benny Diep <bennythanh280198@gmail.com>
2024-01-17 20:55:13 +1000

    feat - get schedules from Tanda

--- sam-app/hello-world/app.ts
+++ sam-app/hello-world/app.ts
@@ -10,13 +10,13 @@ import axios from 'axios';
  *
  */
 
-const NO_CREDS = {};
+const TIMEFRAME = 6;
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         const email = process.env.EMAIL;
         const psd = process.env.PASSWORD;
-        const authenticateUrl = `${process.env.TANDA_BASE_URL}/oauth/token`;
+        const authUrl = `${process.env.TANDA_BASE_URL}/oauth/token`;
 
         if (!email || !psd) {
             return {
@@ -28,25 +28,56 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         }
 
         // 1. Auth to Tanda
-        const tandaAccessToken = await axios({
+        const authRes = await axios({
             method: 'post',
-            url: authenticateUrl,
+            url: authUrl,
             headers: {
                 'Cache-Control': 'no-cache',
             },
             data: {
                 username: email,
                 password: psd,
-                scope: 'me',
+                scope: 'roster',
                 grant_type: 'password',
             },
         });
 
-        // GET shifts from today -> next SUN
+        if (authRes.statusText == 'error') throw 'Auth failed.';
 
-        console.log(tandaAccessToken.statusText); // OK, error
+        // GET shifts from today -> next SUN
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = (1 + date.getMonth()).toString().padStart(2, '0');
+        const fromDay = date.getDate().toString().padStart(2, '0');
+        const toDay = (date.getDate() + TIMEFRAME).toString().padStart(2, '0');
+
+        const fromDate = `${year}-${month}-${fromDay}`;
+        const toDate = `${year}-${month}-${toDay}`;
+        const shiftsUrl = `${process.env.TANDA_BASE_URL}/v2/schedules?from=${fromDate}&to=${toDate}`;
+        const authHeader = `bearer ${authRes.data.access_token}`;
+
+        console.log(authRes.data);
+        console.log(authHeader);
+
+        const shiftsRes = await axios({
+            method: 'get',
+            url: shiftsUrl,
+            headers: {
+                Authorization: authHeader,
+            },
+        });
 
         // Loop through results -> create events accordingly
+        // shiftsRes.data
+        // keys -> start, finish
+        // default values ->
+        //      title - Quest Hotel Housekeeping
+        //      notification - 6pm night before, 2 hours before start
+        //      tag color - red
+
+        console.log(shiftsRes);
+        // ------------------------------------- PART 2 -----------------------------------
+        // TODO: CREATE CALENDAR EVENT WITH ABOVE ATTRIBUTES, CHECK IF THERES BATCH CREATE
         // POST with await for each requests
         // POST with batch if possible.
 
@@ -61,7 +92,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'Failed to schedule up your next shifts.' + err,
+                message: 'Failed to schedule up your next shifts.',
             }),
         };
     }
